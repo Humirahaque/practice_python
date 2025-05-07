09e8e6b797b12ad234ba52ff02d3746dc17a615f
humirahaque <humirahaque645@gmail.com>
2025-05-07 12:53:04 +0600

    added quicksort

--- Exwrcisw3.py
+++ Exwrcisw3.py
@@ -1,1 +1,14 @@
-print(3+5)
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+arr = [1,5,3,9,2]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
